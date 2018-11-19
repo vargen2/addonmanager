a47c03d7cd06947399ceb6f053eb9325f02d922e
vargen2 <hjaarn2@gmail.com>
2018-11-19 10:14:53 +0100

    fixed test

--- src/test/java/addonmanager/app/net/DownloadAddonTest.java
+++ src/test/java/addonmanager/app/net/DownloadAddonTest.java
@@ -11,28 +11,25 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DownloadAddonTest {
 
     @Test
     void downLoadFile() {
-        Addon addon= App.getFactory().createAddon("Details","");
+        Addon addon = App.getFactory().createAddon(App.getFactory().createGame("", "", ""), "Details", "");
         addon.setProjectUrl("https://wow.curseforge.com/projects/details");
-        Download download=new Download("release","v8.0.1.6600.135","4.15 MB",null,null,0,"https://wow.curseforge.com/projects/details/files/2629656/download");
-//        download.fileSize="4.15 MB";
-//        download.downloadLink="https://wow.curseforge.com/projects/details/files/2629656/download";
-//        download.title="v8.0.1.6600.135";
-//        download.release="release";
-        List<Download> downloads=new ArrayList<>();
+        Download download = new Download("release", "v8.0.1.6600.135", "4.15 MB", null, null, 0, "https://wow.curseforge.com/projects/details/files/2629656/download");
+        List<Download> downloads = new ArrayList<>();
         downloads.add(download);
         addon.setDownloads(downloads);
-        File zipFile=DownloadAddon.downLoadFile(addon,addon.getLatestDownload());
+        File zipFile = DownloadAddon.downLoadFile(addon, addon.getLatestDownload());
 
         assertTrue(zipFile.exists());
 
         try {
-            assertEquals(4351416,Files.size(zipFile.toPath()));
+            assertEquals(4351416, Files.size(zipFile.toPath()));
         } catch (IOException e) {
             e.printStackTrace();
         }

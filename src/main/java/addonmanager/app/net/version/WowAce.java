dacb4814badb10906872b5a4736c417b11a46d9b
hjalmar-arnell <hjalmararnell@gmail.com>
2018-12-27 11:53:03 +0100

    fix for wowace bug
    keep 1 global httpclient seems to fix 403 error

--- src/main/java/addonmanager/app/net/version/WowAce.java
+++ src/main/java/addonmanager/app/net/version/WowAce.java
@@ -16,6 +16,8 @@ import java.util.regex.Pattern;
 
 class WowAce extends VersionDownloader {
 
+    private static final HttpClient httpClient = HttpClient.newHttpClient();
+
     WowAce(Addon addon) {
         super(addon);
     }
@@ -28,31 +30,44 @@ class WowAce extends VersionDownloader {
         String filePage = "";
         if (page != 0)
             filePage = "?page=" + page;
-        //updateable.updateProgress(0.1,1);
-        String retrying = "retrying";
+
         String input = "";
 
-        HttpClient httpClient = HttpClient.newHttpClient();
+        //HttpClient httpClient = HttpClient.newHttpClient();
         URI uri;
         try {
             uri = URI.create(addon.getProjectUrl() + "/files" + filePage);
+            System.out.println(uri);
 
         } catch (IllegalArgumentException e) {
             return downloads;
         }
 
-        HttpRequest request = HttpRequest.newBuilder().uri(uri).build();
+        HttpRequest request = HttpRequest.newBuilder().header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36").uri(uri).build();
         HttpResponse<String> response = null;
+
         try {
-            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
+            int i = 0;
+            do {
+                response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+                if (response.statusCode() != 200) {
+                    String msg = "DL " + i + " fail " + response.statusCode() + " " + addon.getProjectUrl();
+                    App.LOG.info(msg);
+                    updateable.updateMessage(msg);
+                }
+                i++;
+            } while (i < 5 && response.statusCode() != 200);
+            System.out.println(response.headers());
+            if (response.sslSession().isPresent()) {
+                for (String valueName : response.sslSession().get().getValueNames()) {
+                    System.out.println(valueName);
+                }
+            }
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
         if (response.statusCode() != 200) {
-            App.LOG.fine("DL fail " + addon.getProjectUrl());
-
+            App.LOG.info("DL fail " + response.statusCode() + " " + addon.getProjectUrl());
             updateable.updateMessage("DL fail " + addon.getProjectUrl());
             return downloads;
 
